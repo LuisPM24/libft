21707d2e4ae1787910677d1a629abc857aaae7e0
lpalomin <lpalomin@student.42madrid.com>
2025-01-16 17:31:39 +0100

    ft_toupper and ft_tolower

---
+++ ft_tolower.c
@@ -0,0 +1,6 @@
+int	ft_tolower(int c)
+{
+	if (c >= 97 && c <= 122)
+		return (c + 32);
+	return (c);
+}
--- libft.h
+++ libft.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   libft.h                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: lpalomin <lpalomin@student.42madrid.c      +#+  +:+       +#+        */
+/*   By: lpalomin <lpalomin@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/14 11:48:24 by lpalomin          #+#    #+#             */
-/*   Updated: 2025/01/14 11:48:27 by lpalomin         ###   ########.fr       */
+/*   Updated: 2025/01/16 17:26:45 by lpalomin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,5 +27,8 @@ void	*ft_memcpy(void *dest, const void *src, size_t n);
 void	*ft_memmove(void *dest, const void *src, size_t n);
 size_t	ft_strlcpy(char *dst, const char *src, size_t size);
 size_t	ft_strlcat(char *dst, const char *src, size_t size);
+int		ft_toupper(int c);
+int		ft_tolower(int c);
+char	*ft_strchr(const char *s, int c);
 
 #endif
